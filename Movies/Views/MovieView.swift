4c8509df38b6a32a1f1035efac76bc27b37fe0ff
Alistair Fraser <afraser@lcs.on.ca>
2023-04-21 23:56:43 -0400

    Created project and completed initial organization of views.

---
+++ Movies/Views/MovieView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieView.swift
+//  Movies
+//
+//  Created by Alistair Fraser on 2023-04-21.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieView()
+    }
+}
