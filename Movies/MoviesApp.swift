a0da12d07358214067fe08c8e803a6c6350583b4
Alistair Fraser <afraser@lcs.on.ca>
2023-04-21 23:55:37 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Alistair Fraser on 2023-04-21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
