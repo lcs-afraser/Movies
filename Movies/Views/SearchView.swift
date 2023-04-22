515bcd4f5049edb7e2307865a40faf5146ef5362
Alistair Fraser <afraser@lcs.on.ca>
2023-04-22 00:14:12 -0400

    Created a scrollable list to display found songs.

--- Movies/Views/SearchView.swift
+++ Movies/Views/SearchView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    //MARK: Stored properties
+    
+    //Holds the list of songs returned by our search
+    @State var foundMovies: [Movie] = []
+    
+    //MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(foundMovies, id: \.imdbID) { currentMovie in
+            VStack {
+                Text(currentMovie.title)
+                    .bold()
+                HStack {
+                    Text(currentMovie.IMDBRating)
+                    Text(currentMovie.RottenTomatoes)
+                    Text(currentMovie.MetacriticRating)
+                }
+            }
+        }
     }
 }
 
